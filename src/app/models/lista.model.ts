c1f6dedc27586612237b3c8c0061daeeef61dc00
Cristian-byte <andygign40@gmail.com>
2020-11-28 14:15:10 -0600

    Servicio y Clases de Lista Deseos

---
+++ src/app/models/lista.model.ts
@@ -0,0 +1,19 @@
+import { ListaItem } from './lista-item.models';
+
+export class Lista {
+    id: number;
+    titulo: string;
+    creadoEn: Date;
+    terminadaEn: Date;
+    terminado: boolean;
+    items: ListaItem[];
+
+    constructor( titulo: string ) {
+        this.titulo = titulo;
+        this.creadoEn = new Date();
+        this.terminado = false;
+        this.items = [];
+        this.id = new Date().getTime();
+    }
+
+}
