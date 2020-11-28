65459473b45065b5c420c9aa1e78042dbdc6436c
Cristian-byte <andygign40@gmail.com>
2020-11-28 15:52:55 -0600

    Localstorage

--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -9,15 +9,26 @@ export class DeseosService {
   listas: Lista[] = [];
 
   constructor() {
-    const lista1 = new Lista('Recolectar Gemas del Infinito');
-    const lista2 = new Lista('Heroes Desaparecen');
 
-    this.listas.push(lista1, lista2);
+    this.cargarStorage();
   }
 
   crearLista( titulo: string ) {
 
     const nuevaLista = new Lista(titulo);
     this.listas.push( nuevaLista);
+    this.guardarStorage();
+  }
+
+  guardarStorage() {
+
+    localStorage.setItem( 'data', JSON.stringify( this.listas ) );
+  }
+
+  cargarStorage() {
+
+    if ( localStorage.getItem('data')) {
+      this.listas = JSON.parse( localStorage.getItem( 'data' ) );
+    }
   }
 }
