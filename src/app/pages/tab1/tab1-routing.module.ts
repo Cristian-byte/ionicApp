04a50a3916daefd241c701c63e1ff78220a42f4c
Cristian-byte <andygign40@gmail.com>
2020-11-28 14:54:42 -0600

    Navegacion Entre Pantallas

--- src/app/pages/tab1/tab1-routing.module.ts
+++ src/app/pages/tab1/tab1-routing.module.ts
@@ -3,10 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { Tab1Page } from './tab1.page';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: Tab1Page,
-  }
+  { path: '', component: Tab1Page, },
+  { path: 'agregar', loadChildren: () => import('../agregar/agregar.module').then( m => m.AgregarPageModule) }
 ];
 
 @NgModule({
